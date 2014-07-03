49ae6e181d08b552c55078c589a2496c1db95226
Mark <MLenser@gmail.com>
2014-07-03 12:27:03 +0200

    use proper name of iAttrs
    iAttrs.uiMaskFormat instead of iAttrs.maskFormat

--- modules/mask/mask.js
+++ modules/mask/mask.js
@@ -218,7 +218,7 @@ angular.module('ui.mask', [])
           }
 
           function getMaskFormatChar(i) {
-            var maskFormat = iAttrs.maskFormat;
+            var maskFormat = iAttrs.uiMaskFormat;
 
             if (typeof maskFormat !== 'undefined' && maskFormat[i]) {
               return maskFormat[i];
